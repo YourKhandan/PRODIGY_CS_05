468cabfbd16d85b6441c4c04d5db49276a6c14ba
Debargha Pramanik <debarghapramanik66@gmail.com>
2025-06-20 20:24:58 +0530

    Update run.py
    make run.py Render - compatible

--- netpacsniff/run.py
+++ netpacsniff/run.py
@@ -1,7 +1,8 @@
 from app import create_app
 from app.extensions import socketio
-
+import os
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=7000)
+    port=int(os.environ.get("PORT",70000))
+    socketio.run(app, host="0.0.0.0", port=port)
